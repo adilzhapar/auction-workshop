90a2c28c4a9419e51cf78962ce2c1c357b01bef6
Adil Zhapar <adilzhapar2002@gmail.com>
2023-05-03 10:24:34 +0600

    added permission classes

--- back/back/settings.py
+++ back/back/settings.py
@@ -133,6 +133,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PARSER_CLASSES': [
