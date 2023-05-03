d7e636ab94d1bb8f15c70b052f8151796a56cc90
Adil Zhapar <adilzhapar2002@gmail.com>
2023-05-03 10:34:17 +0600

    permission

--- back/api/views.py
+++ back/api/views.py
@@ -17,7 +17,7 @@ class ItemViewSet(
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
 ):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | ReadOnly]
     authentication_classes = (TokenAuthentication,)
 
     def get_serializer_class(self):
@@ -36,7 +36,7 @@ class ItemOnSaleViewSet(
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
 ):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | ReadOnly]
     authentication_classes = (TokenAuthentication,)
 
     def get_serializer_class(self):
--- back/back/settings.py
+++ back/back/settings.py
@@ -133,10 +133,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PARSER_CLASSES': [
